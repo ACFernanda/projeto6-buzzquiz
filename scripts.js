6e8eade575be9775a58f907f2fb0b677f67673aa
unverzed <vitoriarortega@gmail.com>
2022-02-16 17:34:59 -0300

    feat: button createquizz

---
+++ scripts.js
@@ -0,0 +1,6 @@
+//* Tela 1 -> Tela 3 *//
+
+function createQuizz() {
+    document.querySelector(".quizz-basic-information").classList.remove("hide");
+    document.querySelector(".list-quizzes").classList.add("hide");
+}
