aa8e80751e9afc8a6421d8a68ae2d3c20fe77836
unverzed <vitoriarortega@gmail.com>
2022-02-16 20:38:04 -0300

    feat: requisition for quizzes from server

--- scripts.js
+++ scripts.js
@@ -1,6 +1,33 @@
+let renderQuizz = [];
+
 //* Tela 1 -> Tela 3 *//
 
 function createQuizz() {
     document.querySelector(".quizz-basic-information").classList.remove("hide");
     document.querySelector(".list-quizzes").classList.add("hide");
+}
+
+const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+promise.then(renderQuizzOnScreen);
+promise.catch(errorScreen);
+
+function renderQuizzOnScreen(response) {
+    renderQuizz = response.data;
+    console.log(renderQuizz);
+    const render = document.querySelector(".all-quizzes");
+    console.log(render);
+    
+    for (let i = 0; i < renderQuizz.length; i++) {
+        render.innerHTML += `
+        <article class="all-quizzes">
+        <img src="${renderQuizz[i].image}" class="img-quizzes"> 
+        <h1 class="name-quizzes">${renderQuizz[i].title}<h1>
+        </article>    `
+    }   
+ } 
+ 
+
+function errorScreen(error) {
+    console.log(error.data);
+
 }
