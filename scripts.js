5ed33f01fdd57ba1e458c300c04a310daa95eeb3
acfernanda <acfernanda98@gmail.com>
2022-02-16 20:58:38 -0300

    js page 2 start

--- scripts.js
+++ scripts.js
@@ -4,3 +4,26 @@ function createQuizz() {
   document.querySelector(".quizz-basic-information").classList.remove("hide");
   document.querySelector(".list-quizzes").classList.add("hide");
 }
+
+//* Tela 2*//
+
+function renderOneQuizz() {
+  renderQuizzTitle();
+  renderQuizzQuestions();
+}
+
+function renderQuizzTitle() {
+  const quizzTitleText = document.querySelector(
+    "quizz-title-container span"
+  ).innerHTML;
+  const quizzTitleImg = document.querySelector("img").src;
+}
+
+function renderQuizzQuestions() {}
+
+function restartQuizz() {}
+
+function returnToHomePage() {
+  document.querySelector("playquizz-page").classList.add("hide");
+  document.querySelector(".list-quizzes").classList.remove("hide");
+}
